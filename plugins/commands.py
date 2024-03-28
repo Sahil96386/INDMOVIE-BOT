3066f1b12b4edaac58f69452d1dadd5911c438f5
Sahil96386 <122726309+Sahil96386@users.noreply.github.com>
2024-03-28 12:59:44 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -176,11 +176,6 @@ async def start(client, message):
     if not files_:
         pre, file_id = ((base64.urlsafe_b64decode(data + "=" * (-len(data) % 4))).decode("ascii")).split("_", 1)
         try:
-            # Create the inline keyboard button with callback_data
-            button = InlineKeyboardButton('▶ Gen Stream / Download Link', callback_data=f'generate_stream_link:{file_id}')
-            # Create the inline keyboard markup with the button
-            keyboard = InlineKeyboardMarkup([[button]])
-            
             msg = await client.send_cached_media(chat_id=message.from_user.id, file_id=file_id, reply_markup = keyboard, protect_content=True if pre == 'filep' else False,)
             filetype = msg.media
             file = getattr(msg, filetype)
@@ -206,11 +201,6 @@ async def start(client, message):
             f_caption=f_caption
     if f_caption is None:
         f_caption = f"{files.file_name}"
-
-    button = InlineKeyboardButton('▶ Gen Stream / Download Link', callback_data=f'generate_stream_link:{file_id}')
-    # Create the inline keyboard markup with the button
-    keyboard = InlineKeyboardMarkup([[button]])
-    
     await client.send_cached_media(chat_id=message.from_user.id, file_id=file_id, caption=f_caption, reply_markup=keyboard, protect_content=True if pre == 'filep' else False,)
                     
 
